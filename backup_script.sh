262e4293b0761e2ebf160670c351098795c98313
Dreyhh <d.reyhh13@gmail.com>
2023-10-04 18:27:32 -0600

    Remove deletion of expired events

--- backup_script.sh
+++ backup_script.sh
@@ -23,9 +23,7 @@ cd /app
 
 FILE_NAME="backup${currentdate}.jsonl"
 LATEST_BACKUP="backup_latest.jsonl"
-LOG_FILE="/var/log/backup_script.log"
 
-touch "$LOG_FILE"
 
 [[ -z "$BUCKET_NAME" ]] && {
     log "Bucket name is empty. Exiting."
@@ -41,12 +39,6 @@ touch "$LOG_FILE"
 if [[ -f "$LATEST_BACKUP" ]]; then
     if diff "$FILE_NAME" "$LATEST_BACKUP" &>/dev/null; then
         log "No changes detected. Exiting."
-    # Upload the log file to the S3 bucket
-    if /usr/local/bin/aws s3 cp "$LOG_FILE" "s3://${BUCKET_NAME}/" ; then
-        log "Log file ${LOG_FILE} has been successfully uploaded to ${BUCKET_NAME}."
-    else
-        log "Failed to upload log file ${LOG_FILE} to ${BUCKET_NAME}."
-    fi
         rm -f "$FILE_NAME"
         exit 0
     fi
@@ -55,13 +47,6 @@ fi
 # Upload the file to the S3 bucket if different or if latest backup doesn't exist
 if /usr/local/bin/aws s3 cp "$FILE_NAME" "s3://${BUCKET_NAME}/${POD_NAME}/${POD_NAME}${FILE_NAME}" ; then
     log "File ${FILE_NAME} has been successfully uploaded to ${BUCKET_NAME}."
-
-     # Upload the log file to the S3 bucket
-    if /usr/local/bin/aws s3 cp "$LOG_FILE" "s3://${BUCKET_NAME}/${POD_NAME}/${POD_NAME}backup.log" ; then
-        log "Log file ${LOG_FILE} has been successfully uploaded to ${BUCKET_NAME}."
-    else
-        log "Failed to upload log file ${LOG_FILE} to ${BUCKET_NAME}."
-    fi
 
     # Update latest backup and remove the original file
     mv -f "$FILE_NAME" "$LATEST_BACKUP"
--- src/apps/relay/RelayCron.cpp
+++ src/apps/relay/RelayCron.cpp
@@ -66,62 +66,6 @@ void RelayServer::runCron() {
         }
     });
 
-
-    // Delete expired events
-
-    cron.repeat(9 * 1'000'000UL, [&]{
-        std::vector<uint64_t> expiredLevIds;
-        uint64_t numEphemeral = 0;
-        uint64_t numExpired = 0;
-
-        {
-            auto txn = env.txn_ro();
-
-            auto mostRecent = getMostRecentLevId(txn);
-            uint64_t now = hoytech::curr_time_s();
-            uint64_t ephemeralCutoff = now - cfg().events__ephemeralEventsLifetimeSeconds;
-
-            env.generic_foreachFull(txn, env.dbi_Event__expiration, lmdb::to_sv<uint64_t>(0), lmdb::to_sv<uint64_t>(0), [&](auto k, auto v) {
-                auto expiration = lmdb::from_sv<uint64_t>(k);
-                auto levId =  lmdb::from_sv<uint64_t>(v);
-
-                if (levId == mostRecent) return true;
-
-                if (expiration == 1) { // Ephemeral event
-                    auto view = env.lookup_Event(txn, levId);
-                    if (!view) throw herr("missing event from index, corrupt DB?");
-                    uint64_t created = view->flat_nested()->created_at();
-
-                    if (created <= ephemeralCutoff) {
-                        numEphemeral++;
-                        expiredLevIds.emplace_back(levId);
-                    }
-                } else {
-                    numExpired++;
-                    expiredLevIds.emplace_back(levId);
-                }
-
-                return expiration <= now;
-            });
-        }
-
-        if (expiredLevIds.size() > 0) {
-            auto txn = env.txn_rw();
-
-            uint64_t numDeleted = 0;
-
-            for (auto levId : expiredLevIds) {
-                if (deleteEvent(txn, levId)) numDeleted++;
-            }
-
-            txn.commit();
-
-            if (numDeleted) LI << "Deleted " << numDeleted << " events (ephemeral=" << numEphemeral << " expired=" << numExpired << ")";
-        }
-    });
-
-
-
     cron.run();
 
     while (1) std::this_thread::sleep_for(std::chrono::seconds(1'000'000));
